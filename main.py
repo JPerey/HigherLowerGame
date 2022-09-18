552a4dec2569d99f4894116bf54ca9d6b5d872a2
Jam Jam James Perey <jamjam.perey@gmail.com>
2022-09-18 01:03:48 -0700

    final commit: for now, game is complete

--- main.py
+++ main.py
@@ -5,63 +5,9 @@ import os
 import random
 
 # global variables (if any)
-
+player_score = 0
 
 # function definitions
-
-def higher_lower():
-    # local variables inits (if any)
-    player_score = 0
-    player_choice_correct = False
-    who_has_more = ""
-
-    print(art.logo)
-    pvp_list = fill_pvp()
-    display(pvp_list, player_score)
-    who_has_more = more_followers(pvp_list)
-
-    while player_choice_correct != True:
-        player_AB_choice = input("Who has more followers? A or B: ").upper()
-        if player_AB_choice == "A" or player_AB_choice == "B":
-            player_choice_correct = True
-        else:
-            print("Invalid input. 'A' or 'B' only. ")
-
-    if who_has_more == "A" and player_AB_choice == "A":
-        player_score += 1
-        print(f"correct.")
-    elif who_has_more == "B" and player_AB_choice == "B":
-        player_score += 1
-        print(f"correct.")
-    else:
-        print(f"incorrect. game over. final score: {player_score}")
-    start()
-
-
-def fill_pvp():
-    filled_pvp = ["", ""]
-    for i in range(0, 2):
-        random_choice = random.choice(range(0, len(game_data.data)))
-        #print(f"ranbdom choice: {random_choice}")
-        filled_pvp[i] = game_data.data[random_choice]
-        #print(f"filled pvp: {filled_pvp}")
-    return filled_pvp
-
-
-def display(pvp_list, player_score):
-    print(f"player score: {player_score}")
-    print(
-        f"{pvp_list[0]['name']}, a {pvp_list[0]['description']}, from {pvp_list[0]['country']}")
-    print(art.vs)
-    print(
-        f"{pvp_list[1]['name']}, a {pvp_list[1]['description']}, from {pvp_list[1]['country']}")
-
-
-def more_followers(pvp_list):
-    if pvp_list[0]['follower_count'] > pvp_list[1]['follower_count']:
-        return "A"
-    else:
-        return "B"
 
 
 def clear():
@@ -71,11 +17,89 @@ def clear():
     os.system(command)
 
 
-def start():
+def higher_lower():
+    # local variables inits (if any)
+    player_score = 0
+    player_choice_correct = False
+    who_has_more = ""
+    winning = True
+    winning_ticker = 0
+    previous_champ = {}
+
+    while winning == True:
+        print(art.logo)
+        pvp_list = fill_pvp(winning_ticker, previous_champ)
+        previous_champ = pvp_list[1]
+        display(pvp_list, player_score)
+        who_has_more = more_followers(pvp_list)
+        player_choice_correct = False
+
+        while player_choice_correct != True:
+            player_AB_choice = input(
+                "Who has more followers? A or B: ").upper()
+            if player_AB_choice == "A" or player_AB_choice == "B":
+                player_choice_correct = True
+            else:
+                print("Invalid input. 'A' or 'B' only. ")
+
+        if who_has_more == "A" and player_AB_choice == "A":
+            player_score += 1
+            winning_ticker += 1
+        elif who_has_more == "B" and player_AB_choice == "B":
+            player_score += 1
+            winning_ticker += 1
+        elif who_has_more == "Tie":
+            winning_ticker += 1
+        else:
+            winning = False
+    start(player_score)
+
+
+def fill_pvp(winning_ticker, previous_champ):
+    filled_pvp = ["", ""]
+    if winning_ticker == 0:
+        for i in range(0, 2):
+            random_choice = random.choice(range(0, len(game_data.data)))
+            #print(f"ranbdom choice: {random_choice}")
+            filled_pvp[i] = game_data.data[random_choice]
+            #print(f"filled pvp: {filled_pvp}")
+    else:
+        filled_pvp[0] = previous_champ
+        random_choice = random.choice(range(0, len(game_data.data)))
+        filled_pvp[1] = game_data.data[random_choice]
+
+    return filled_pvp
+
+
+def display(pvp_list, player_score):
+    clear()
+    print(art.logo)
+    print(f"player score: {player_score}")
+    print(
+        f"{pvp_list[0]['name']}, a {pvp_list[0]['description']}, from {pvp_list[0]['country']}")
+    print(f"amount of followers: {pvp_list[0]['follower_count']}")
+    print(art.vs)
+    print(
+        f"{pvp_list[1]['name']}, a {pvp_list[1]['description']}, from {pvp_list[1]['country']}")
+    print(f"amount of followers: {pvp_list[1]['follower_count']}")
+
+
+def more_followers(pvp_list):
+    if pvp_list[0]['follower_count'] > pvp_list[1]['follower_count']:
+        return "A"
+    elif pvp_list[0]['follower_count'] == pvp_list[1]['follower_count']:
+        return "Tie"
+    else:
+        return "B"
+
+
+def start(player_score):
     # local variables inits (if any)
     player_start_correct = False
 
     print(art.logo)
+    if player_score != 0:
+        print(f"final score: {player_score}")
     while player_start_correct != True:
         player_choice = input("Would you like to play? yes or no: ").lower()
         if player_choice == "yes" or player_choice == "no":
@@ -92,4 +116,4 @@ def start():
 
 # timeline
 
-start()
+start(player_score)
